1a2b5fb10f313d463d0b41b92767286982d2b847
woidzero <woidzero@skiff.com>
2023-09-26 20:06:12 +0300

    1.1.0: Version and project data update

---
+++ grabify/__init__.py
@@ -0,0 +1,12 @@
+"""
+Grabify
+~~~~~~~~~~~~~~~~~~~
+
+🎼 A command-line tool that allows you to download artwork and metadata from Spotify 
+tracks and albums without authentication.
+
+:copyright: (c) 2023 woidzero
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "1.1.0"
